5cba4b89f9d30078132b32da791eb08e942eed24
mikepitre <michaelepitre@gmail.com>
2015-10-17 13:42:59 -0500

    added logic for hiding/showing proper UI elements, updating math on every add press

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -11,16 +11,37 @@ import UIKit
 class ViewController: UIViewController {
 
     //Outlets
+    
     @IBOutlet weak var mathLabel: UILabel!
     @IBOutlet weak var multipleTxtField: UITextField!
     @IBOutlet weak var playBtn: UIButton!
     @IBOutlet weak var addBtn: UIButton!
     @IBOutlet weak var titleLabel: UIImageView!
+    
+    //variables
+    
+    var currentTotal = 0
+    var multiple = 0
+    
+    //actions
 
     @IBAction func playBtnPressed(sender: UIButton) {
+        //if input was given set multiple, else do nothing
+        if multipleTxtField.text != "" && multipleTxtField.text != nil {
+            multiple = Int(multipleTxtField.text!)!
+            // hide welcome page elements, show math page elements
+            titleLabel.hidden = true
+            multipleTxtField.hidden = true
+            playBtn.hidden = true
+            mathLabel.hidden = false
+            addBtn.hidden = false
+        } 
     }
     
     @IBAction func addBtnPressed(sender: UIButton) {
+        //show math, update to next multiple with every press
+        mathLabel.text = "\(currentTotal) + \(multiple) = \(currentTotal + multiple)"
+        currentTotal += multiple
     }
     
 
